c383924c3c59871f8de6675223c31756ac4c0268
marourou2016 <marourou2016@icloud.com>
2016-03-02 19:59:51 +0800

    修改api访问路径，获取当前项目的目录

--- routes/note.js
+++ routes/note.js
@@ -2,20 +2,19 @@
  * Created by Administrator on 2016/1/13.
  */
 var express = require('express');
+var path = require('path'); 
 var router = express.Router();
 var User = require('../models/NotesDao.js');
 /* 后台 */
-
 router.get('/', function(req, res, next) {
     //TODO 怎么获取应用的根目录
     var sBaseUrl = req.baseUrl;
-    res.sendFile('E:\\myNode\\public' + sBaseUrl + '\\index.html');
+    var projectBaseUrl = process.cwd();//当前项目的路径
+    res.sendFile(projectBaseUrl+'\\public'+sBaseUrl+'\\index.html');
 });
 
 router.get('/:name', function(req, res, next) {
     var name = req.params.name;
-    
-    
     res.render('APIdetail', { 
     	title: 'Express', 
     	name: name, 
@@ -24,5 +23,4 @@ router.get('/:name', function(req, res, next) {
 
     });
 });
-
 module.exports = router;
